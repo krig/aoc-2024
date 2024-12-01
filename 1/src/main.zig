76c2a200ccd9214bd58a83b91709e959905dfbd2
Kristoffer Grönlund <k@ziran.se>
2024-12-01 22:14:31 +0100

    no need to store the allocator

--- 1/src/main.zig
+++ 1/src/main.zig
@@ -5,13 +5,13 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     const a = arena.allocator();
 
-    var leftList = std.ArrayList(i32).init(a);
-    var rightList = std.ArrayList(i32).init(a);
+    var leftList = std.ArrayListUnmanaged(i32).empty;
+    var rightList = std.ArrayListUnmanaged(i32).empty;
     var it = std.mem.tokenizeScalar(u8, input, '\n');
     while (it.next()) |line| {
         var it2 = std.mem.tokenizeScalar(u8, line, ' ');
-        try leftList.append(try std.fmt.parseInt(i32, it2.next() orelse unreachable, 10));
-        try rightList.append(try std.fmt.parseInt(i32, it2.next() orelse unreachable, 10));
+        try leftList.append(a, try std.fmt.parseInt(i32, it2.next() orelse unreachable, 10));
+        try rightList.append(a, try std.fmt.parseInt(i32, it2.next() orelse unreachable, 10));
     }
 
     std.mem.sort(i32, leftList.items, {}, comptime std.sort.asc(i32));
